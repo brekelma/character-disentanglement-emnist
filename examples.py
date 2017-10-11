aac713da3a6a04f79df478d29ef63134455ef4ba
brekelma <brekelma@usc.edu>
2017-10-11 16:18:38 -0700

    Update examples.py

--- examples.py
+++ examples.py
@@ -9,7 +9,7 @@ letters = emnist.top_letters_by_position(top_words, n = 8)
 print('Length ', length, ' Words combined using letters: ')
 print(letters)
 train_words, test_words = emnist.valid_words_from_letters(letters)
-x_train, y_train = emnist.get_data(train_words, data = 'train', per_word = 1, resample_letters = 'none', save_all_imgs = True)
+x_train, y_train = emnist.get_data(train_words, data = 'train', per_word = 1, resample_letters = 'none', save_all = True)
 x_test, y_test = emnist.get_data(test_words, data = 'test', per_word = 1, resample_letters = 'none')
 
 # primitive example using top 300 occuring words, sampled according to occurence frequency with new letter samples for each word.
@@ -17,5 +17,5 @@ length = 6
 emnist = EMNIST()
 top_words, probabilities = emnist.top_words_of_length(length, max_words=300, return_probabilities = True)
 samples = 500
-x_train, y_train = emnist.get_data(top_words, per_word = np.ceil(probabilities*samples).astype(int), resample_letters = 'words', save_all_imgs = True)
+x_train, y_train = emnist.get_data(top_words, per_word = np.ceil(probabilities*samples).astype(int), resample_letters = 'words')
 print(x_train.shape) # note, we have fewer than 500 samples due to rounding to integers
