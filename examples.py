ceb7d6727a754bf0b03ef467467dc71b62478e90
brekelma <brekelma@usc.edu>
2017-10-11 16:21:43 -0700

    Update examples.py

--- examples.py
+++ examples.py
@@ -12,10 +12,11 @@ train_words, test_words = emnist.valid_words_from_letters(letters)
 x_train, y_train = emnist.get_data(train_words, data = 'train', per_word = 1, resample_letters = 'none', save_all = True)
 x_test, y_test = emnist.get_data(test_words, data = 'test', per_word = 1, resample_letters = 'none')
 
-# primitive example using top 300 occuring words, sampled according to occurence frequency with new letter samples for each word.
+# example using top 300 occuring words, sampled according to occurence frequency with new letter samples for each word.
 length = 6
 emnist = EMNIST()
 top_words, probabilities = emnist.top_words_of_length(length, max_words=300, return_probabilities = True)
 samples = 500
-x_train, y_train = emnist.get_data(top_words, per_word = np.ceil(probabilities*samples).astype(int), resample_letters = 'words')
+# resample_letters = 'words' means use, for example, the same letter p for both instances in word "pip", with different samples in each per_word and across words
+x_train, y_train = emnist.get_data(top_words, per_word = np.round(probabilities*samples).astype(int), resample_letters = 'words')
 print(x_train.shape) # note, we have fewer than 500 samples due to rounding to integers
